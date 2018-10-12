db200d3b30d7187055a2445751fffbb0ec5a4ced
jsrduck <jordanrudd@outlook.com>
2018-10-12 13:25:23 -0700

    Make Ruddy.exe fully inclusive, no need for batch file
    * llc is now built into Ruddy.exe. This was done by copying llc.cpp and
    slightly modifying it.
    * VS2017 link.exe is still used for linking. In the future, would like
    to build lld directly into Ruddy and use that, but it's currently not an
    available package on vcpkg. We now call link.exe in-proc with a call to
    CreateProcess

--- Ruddy/Ruddy.cpp
+++ Ruddy/Ruddy.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h"
+
 #include <Grammar.h>
 #include <Statements.h>
 #include <Parser.h>
@@ -18,6 +19,8 @@
 #include <llvm\IR\IRBuilder.h>
 #include <llvm\Support\raw_ostream.h>
 #include <llvm\Support\FileSystem.h>
+
+using namespace std;
 
 inline std::unique_ptr<Ast::GlobalStatements> ParseTree(std::string code)
 {
@@ -45,6 +48,197 @@ void AddExternOsFunctions(llvm::Module* module, llvm::LLVMContext& context)
 	printfArgTypes.push_back(llvm::Type::getInt16PtrTy(context));
 	auto printfFunctionType = llvm::FunctionType::get(llvm::Type::getInt32Ty(context), printfArgTypes, true /*isVarArg*/);
 	auto printfFunction = llvm::Function::Create(printfFunctionType, llvm::Function::ExternalLinkage, "_os_printf", module);
+}
+
+int llc(int argc, char **argv);
+int LLC(std::string &outputFileName, llvm::LLVMContext& context)
+{
+	//-filetype = obj
+	char* args[3];
+
+	char* llcName = "llc";
+	std::string arg1 = "-filetype=obj";
+	char arg1Buff[_MAX_PATH];
+	if (strncpy_s(arg1Buff, _countof(arg1Buff), arg1.c_str(), arg1.size()+1) != 0)
+	{
+		return -11;
+	}
+
+	char arg2Buff[_MAX_PATH];
+	if (strncpy_s(arg2Buff, _countof(arg2Buff), outputFileName.c_str(), outputFileName.size() + 1) != 0)
+	{
+		return -12;
+	}
+
+	args[0] = llcName;
+	args[1] = arg1Buff;
+	args[2] = arg2Buff;
+
+	return llc(3, args);
+}
+
+class CoInitializer
+{
+public:
+	CoInitializer()
+	{
+		hr = ::CoInitialize(NULL);
+		if (FAILED(hr))
+		{
+			throw std::exception("failed to initialize COM");
+		}
+	}
+
+	~CoInitializer()
+	{
+		if (SUCCEEDED(hr))
+		{
+			::CoUninitialize();
+		}
+	}
+
+private:
+	HRESULT hr;
+};
+
+
+// Use smart pointers (without ATL) to release objects when they fall out of scope.
+_COM_SMARTPTR_TYPEDEF(ISetupInstance, __uuidof(ISetupInstance));
+_COM_SMARTPTR_TYPEDEF(ISetupInstance2, __uuidof(ISetupInstance2));
+_COM_SMARTPTR_TYPEDEF(IEnumSetupInstances, __uuidof(IEnumSetupInstances));
+_COM_SMARTPTR_TYPEDEF(ISetupConfiguration, __uuidof(ISetupConfiguration));
+_COM_SMARTPTR_TYPEDEF(ISetupConfiguration2, __uuidof(ISetupConfiguration2));
+_COM_SMARTPTR_TYPEDEF(ISetupHelper, __uuidof(ISetupHelper));
+_COM_SMARTPTR_TYPEDEF(ISetupPackageReference, __uuidof(ISetupPackageReference));
+_COM_SMARTPTR_TYPEDEF(ISetupPropertyStore, __uuidof(ISetupPropertyStore));
+_COM_SMARTPTR_TYPEDEF(ISetupInstanceCatalog, __uuidof(ISetupInstanceCatalog));
+int GetCurrentVSInstance(BSTR &installPath)
+{
+	CoInitializer init;
+	ISetupConfigurationPtr query;
+
+	auto hr = query.CreateInstance(__uuidof(SetupConfiguration));
+	if (REGDB_E_CLASSNOTREG == hr)
+	{
+		std::cout << "Could not find instance of VS on this machine, cannot link files." << endl;
+		return -7;
+	}
+	else if (FAILED(hr))
+	{
+		std::cout << "Unexpected error: could not find instance of VS on this machine, cannot link files." << endl;
+		return -8;
+	}
+
+	ISetupConfiguration2Ptr query2(query);
+	IEnumSetupInstancesPtr e;
+
+	hr = query2->EnumAllInstances(&e);
+	if (FAILED(hr))
+	{
+		std::cout << "Unexpected error: could not find instance of VS on this machine, cannot link files." << endl;
+		return -9;
+	}
+
+	ISetupHelperPtr helper(query);
+
+	ISetupInstance* pInstances[1] = {};
+	hr = e->Next(1, pInstances, NULL);
+	if (FAILED(hr))
+	{
+		std::cout << "Unexpected error: could not find instance of VS on this machine, cannot link files." << endl;
+		return -10;
+	}
+	while (S_OK == hr)
+	{
+		// Wrap instance without AddRef'ing.
+		ISetupInstancePtr instance = ISetupInstancePtr(pInstances[0], false);
+		instance->GetInstallationPath(&installPath);
+		hr = e->Next(1, pInstances, NULL);
+	}
+	return 0;
+}
+
+int ExecuteExe(std::wstring process, std::wstring& args)
+{
+	wchar_t argsBuff[2047];
+	if (wcsncpy_s(argsBuff, args.c_str(), _countof(argsBuff)) != 0)
+	{
+		return -6;
+	}
+
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	si.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+	si.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	ZeroMemory(&pi, sizeof(pi));
+	if (!!::CreateProcess(process.c_str(), argsBuff, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+	{
+		WaitForSingleObject(pi.hProcess, INFINITE);
+		DWORD exitCode;
+		if (!!::GetExitCodeProcess(pi.hProcess, &exitCode))
+		{
+			if (exitCode != 0)
+			{
+				return exitCode;
+			}
+		}
+		else
+		{
+			return -4;
+		}
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+	}
+	else
+	{
+		return -5;
+	}
+
+	return 0;
+}
+
+int RunLinker(std::string& objFileName, std::string& outputName)
+{
+	// Get current working directory
+	wchar_t workingDir[_MAX_PATH];
+
+	if (!_wgetcwd(workingDir, _countof(workingDir)))
+	{
+		std::cerr << "Failed to get current working dir" << endl;
+		return errno;
+	}
+
+	wchar_t comSpecBuffer[_MAX_PATH];
+	size_t reqCount;
+	if (_wgetenv_s(&reqCount, comSpecBuffer, L"ComSpec") != 0)
+	{
+		return -2;
+	}
+	std::wstring cmdExePath(comSpecBuffer);
+
+	// Set VS env variables
+	if (_wputenv_s(L"VSCMD_START_DIR", workingDir) != 0)
+	{
+		return -3;
+	}
+
+	// Get VS install dir
+	BSTR installDir;
+	int retval = GetCurrentVSInstance(installDir);
+	if (retval != 0)
+		return retval;
+
+	std::wstring linkCmdPath(cmdExePath);
+	linkCmdPath.append(L" /c \"");
+	linkCmdPath.append(installDir);
+	linkCmdPath.append(L"\\VC\\Auxiliary\\Build\\vcvarsall.bat\" x86 & link ");
+	linkCmdPath.append(objFileName.begin(), objFileName.end());
+	linkCmdPath.append(L" ..\\WindowsImpl\\Debug\\IO.obj ..\\WindowsImpl\\Debug\\stdafx.obj /subsystem:console /OUT:");
+	linkCmdPath.append(outputName.begin(), outputName.end());
+
+	return ExecuteExe(cmdExePath, linkCmdPath);
 }
 
 // Ruddy.exe produces the LLVM intermediate code representation. A batch file can be used as a full end-to-end compiler
@@ -92,16 +286,16 @@ int main(int argc, char* argv[])
 		auto symbolTable = std::make_shared<Ast::SymbolTable>();
 
 		// Code generation
-		llvm::LLVMContext* TheContext = new llvm::LLVMContext();
-		llvm::IRBuilder<> builder(*TheContext);
-		llvm::Module* module = new llvm::Module("Module", *TheContext);
+		llvm::LLVMContext TheContext {};
+		llvm::IRBuilder<> builder(TheContext);
+		llvm::Module module("Module", TheContext);
 		// Generate extern statements for os code. In the future, this will be
 		// replaced by import statements so we're not generating everything
-		AddExternOsFunctions(module, *TheContext);
+		AddExternOsFunctions(&module, TheContext);
 		// Generate code from the input
 		masterList->TypeCheck(symbolTable);
 
-		masterList->CodeGen(&builder, TheContext, module);
+		masterList->CodeGen(&builder, &TheContext, &module);
 
 		// Now save it to a file
 		std::string fileName = argv[1];
@@ -110,17 +304,32 @@ int main(int argc, char* argv[])
 
 		std::error_code errInfo;
 		auto stream = std::make_unique<llvm::raw_fd_ostream>(outputFileName, errInfo, llvm::sys::fs::OpenFlags::F_RW);
-		module->print(*stream.get(), nullptr);
+		module.print(*stream.get(), nullptr);
 		stream->close();
+
+		auto retVal = LLC(outputFileName, TheContext);
+		if (retVal != 0)
+			return retVal;
+
+		auto objFileName = fileName.substr(0, fileName.find_last_of('.'));
+		auto exeFileName = objFileName;
+		objFileName.append(".obj");
+		exeFileName.append(".exe");
+		retVal = RunLinker(objFileName, exeFileName);
+		if (retVal != 0)
+		{
+			std::cerr << "Failed to execute link.exe, cannot produce link files: " << GetLastError() << endl;
+			return retVal;
+		}
 	}
 	catch (Ast::Exception& e)
 	{
-		std::cout << e.Message();
+		std::cerr << e.Message() << endl;
 		return -1;
 	}
 	catch (std::exception& e)
 	{
-		std::cout << "Unhandled exception, bad Ruddy, bad: " << e.what();
+		std::cerr << "Unhandled exception, bad Ruddy, bad: " << e.what() << endl;
 		return -1;
 	}
 	
--- Ruddy/stdafx.h
+++ Ruddy/stdafx.h
@@ -7,9 +7,29 @@
 
 #include "targetver.h"
 
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include <stdio.h>
 #include <tchar.h>
+#include <stdlib.h>
 
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <windows.h>
+
+#include <crtdbg.h>
+#include <comip.h>
+#include <comdef.h>
+#include <comutil.h>
+#include <atlbase.h>
+#include <atlsafe.h>
+
+#include <Setup.Configuration.h>
+
+#include <processthreadsapi.h>
 
 
 // TODO: reference additional headers your program requires here
